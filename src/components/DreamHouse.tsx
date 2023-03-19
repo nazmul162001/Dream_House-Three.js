2a6f10ccb5eb8a747a99c2a7e5be2a7739d48600
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-19 16:01:05 +0600

    create bushes

--- src/components/DreamHouse.tsx
+++ src/components/DreamHouse.tsx
@@ -61,6 +61,28 @@ const DreamHouse = () => {
     door.position.z = 2 + 0.01 // 2 is the half of the  last value in the BoxGeometry
     house.add(door)
 
+    // Bushes
+    const bushGeometry = new THREE.SphereGeometry(1, 16, 16)
+    const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
+
+    const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush1.scale.set(0.5, 0.5, 0.5)
+    bush1.position.set(0.8, 0.2, 2.2)
+
+    const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush2.scale.set(0.25, 0.25, 0.25)
+    bush2.position.set(1.4, 0.1, 2.1)
+
+    const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush3.scale.set(0.4, 0.4, 0.4)
+    bush3.position.set(-0.8, 0.1, 2.2)
+
+    const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+    bush4.scale.set(0.15, 0.15, 0.15)
+    bush4.position.set(-1, 0.05, 2.6)
+
+    house.add(bush1, bush2, bush3, bush4)
+
     // Floor (create floor)
     const floor = new THREE.Mesh(
       new THREE.PlaneGeometry(20, 20),
